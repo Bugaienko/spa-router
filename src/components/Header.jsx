0f0eac43bbbad5f9402ca563f2cfb30e7e580f62
Bugaienko <sergiibugaienko@gmail.com>
2022-08-02 13:22:38 +0200

    second commit2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,13 +8,13 @@ function Header() {
 		<div className="nav-wrapper">
 			<a href="https://bugaienko.github.io/spa-router" className="brand-logo right">SPA app</a>
 
-			<ul id = "nav-mobile" className = "center hide-on-med-and-down">
+			<ul id = "nav-mobile" className = "center ">
 				<li><CustomLink to="/">Home</CustomLink></li>
 				<li><CustomLink to="/about">About</CustomLink></li>
 				<li><CustomLink to="/contacts">Contacts</CustomLink></li>
 			</ul>
 
-			<ul id="nav-mobile" className="left hide-on-med-and-down">
+			<ul id="nav-mobile" className="left ">
 				<li>
 					<a href="https://github.com/Bugaienko/spa-router" target="_blank" rel="noreferrer">Repo</a>
 				</li>
